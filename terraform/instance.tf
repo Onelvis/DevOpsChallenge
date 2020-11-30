5c3c9418ebe9cf13f33ee545881a276a83be2e6d
onelvis <onelvis96@gmail.com>
2020-11-30 13:03:59 -0400

    Testing something

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -23,15 +23,3 @@ resource "aws_instance" "web" {
     name = "dc-sample"
   }
 }
-
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  # Inbound HTTP from anywhere
-  ingress {
-    from_port   = 5000
-    to_port     = 5001
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
